65cfbba94ad02a3a7ecc3fa626dbbf0e519d7ffa
Vivek Vishwakarma <viveksallu156@gmail.com>
2017-07-13 18:20:02 +0530

    Update turtleshapes.py

--- turtleshapes.py
+++ turtleshapes.py
@@ -1,77 +1,78 @@
-import turtle
-from turtle import color
-my_turtle = turtle.Turtle()
-my_turtle = turtle.color("blue")
-my_turtle =turtle.bgcolor("green")
-my_turtle = turtle.speed(0)
-def square(length, angle):
-    for i in range(4):
-        my_turtle = turtle.forward(length)
-        my_turtle = turtle.right(angle)
-square(100, 90)
-for i in range(150):
-    square(100, 90)
-    my_turtle = turtle.right(11)
 
-
-
-import turtle
-import math
+import turtle #imports turtle library
+import math #imports math library
 import random
+
+
 wn = turtle.Screen()
 wn.bgcolor('fuchsia')
+
 Albert = turtle.Turtle()
 Albert.speed(0)
 Albert.color('white')
 rotate=int(360)
+
 def drawCircles(t,size):
     for i in range(10):
         t.circle(size)
         size=size-4
+        
 def drawSpecial(t,size,repeat):
   for i in range (repeat):
     drawCircles(t,size)
     t.right(360/repeat)
 drawSpecial(Albert,100,10)
+
 Steve = turtle.Turtle()
 Steve.speed(0)
 Steve.color('yellow')
 rotate=int(90)
+
 def drawCircles(t,size):
     for i in range(4):
         t.circle(size)
         size=size-10
+        
 def drawSpecial(t,size,repeat):
     for i in range (repeat):
         drawCircles(t,size)
         t.right(360/repeat)
+        
 drawSpecial(Steve,100,10)
+
 Barry = turtle.Turtle()
 Barry.speed(0)
 Barry.color('blue')
 rotate=int(80)
+
 def drawCircles(t,size):
     for i in range(4):
         t.circle(size)
         size=size-5
+        
 def drawSpecial(t,size,repeat):
     for i in range (repeat):
         drawCircles(t,size)
         t.right(360/repeat)
 drawSpecial(Barry,100,10)
+
 Terry = turtle.Turtle()
 Terry.speed(0)
 Terry.color('orange')
 rotate=int(90)
+
 def drawCircles(t,size):
     for i in range(4):
         t.circle(size)
         size=size-19
+        
 def drawSpecial(t,size,repeat):
     for i in range (repeat):
         drawCircles(t,size)
         t.right(360/repeat)
+        
 drawSpecial(Terry,100,10)
+
 Will = turtle.Turtle()
 Will.speed(0)
 Will.color('pink')
